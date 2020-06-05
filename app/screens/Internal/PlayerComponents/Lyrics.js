bbea5b4b3be7692b1fb210a70eaafd423770eb63
neil-sriv <neil.srivastava1@gmail.com>
2020-06-04 22:55:19 -0500

    auto scroll working, need to work on a formula for song length/lyrics length, also need to work on "on touch complete" and shit to update offset at that point

--- app/screens/Internal/PlayerComponents/Lyrics.js
+++ app/screens/Internal/PlayerComponents/Lyrics.js
@@ -1,34 +1,92 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, Text, StyleSheet } from 'react-native';
+import {
+	SafeAreaView,
+	ScrollView,
+	Text,
+	StyleSheet,
+	FlatList,
+} from 'react-native';
 
 export default class Lyrics extends React.Component {
+	flatList = React.createRef();
 	constructor(props) {
-        super(props);
+		super(props);
 		this.state = {
 			lyrics: this.props.lyrics,
-        };
-    }
+			offset: 0
+		};
+	}
+
+	componentDidMount() {
+		this._stopAutoPlay();
+		this._startAutoPlay();
+	}
+
+	componentWillUnmount() {
+		this._stopAutoPlay();
+	}
+
+	_goToNextPage = () => {
+		this.flatList.current.scrollToOffset({ offset: this.state.offset });
+		this.setState(
+			{ offset: this.state.offset+2 }
+		)
+	};
+
+	_startAutoPlay = () => {
+		this._timerId = setInterval(this._goToNextPage, 200);
+	};
+
+	_stopAutoPlay = () => {
+		if (this._timerId) {
+			clearInterval(this._timerId);
+			this._timerId = null;
+		}
+	};
 
 	render() {
 		return (
+			// <SafeAreaView style={styles.scrollView}>
+			// 	<ScrollView
+			// 		ref={(ref) => {
+			// 			this.scrollView = ref;
+			// 		}}
+			// 		onContentSizeChange={() =>
+			// 			this.scrollView.scrollTo({ x: 0, y: 0, animated: true })
+			// 		}
+			// 	>
+			// 		<Text
+			// 			style={{
+			// 				color: '#2AA7E7',
+			// 				fontSize: 20,
+			// 			}}
+			// 		>
+			// 			{this.state.lyrics}
+			// 		</Text>
+			// 	</ScrollView>
+			// </SafeAreaView>
 			<SafeAreaView style={styles.scrollView}>
-				<ScrollView
-					ref={(ref) => {
-						this.scrollView = ref;
-					}}
-					onContentSizeChange={() =>
-						this.scrollView.scrollTo({ x: 0, y: 0, animated: true })
-					}
-				>
-					<Text
-						style={{
-							color: '#2AA7E7',
-							fontSize: 20,
-						}}
-					>
-						{this.state.lyrics}
-					</Text>
-				</ScrollView>
+				<FlatList
+					flatListRef={React.createRef()}
+					ref={this.flatList}
+					data={[{ id: 1, text: this.state.lyrics }]}
+					// onContentSizeChange={() =>
+					// 	this.flatList.current.scrollToOffset({
+					// 		offset: 100,
+					// 		animated: true,
+					// 	})
+					// }
+					renderItem={({ item }) => (
+						<Text
+							style={{
+								color: '#2AA7E7',
+								fontSize: 20,
+							}}
+						>
+							{this.state.lyrics}
+						</Text>
+					)}
+				/>
 			</SafeAreaView>
 		);
 	}
